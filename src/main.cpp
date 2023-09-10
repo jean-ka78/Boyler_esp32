90bc19e7edfcba9eec13e42cc85a02385b257df2
jean <jean@ukr.net>
2023-09-10 15:37:19 +0200

    up

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ArduinoOTA.h>
 #include "link.h"
 // put function declarations here:
 
-
+unsigned long timer_1;
 int myFunction(int, int);
 
 void setup() {
   Serial.begin(115200);
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-ConnectWIFI();
+  ArduinoOTA.setHostname("ESP32"); // Задаем имя сетевого порта
+  ArduinoOTA.begin(); 
+  
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ArduinoOTA.handle(); // Всегда готовы к прошивке
+
+  if (millis() - timer_1>1000)
+    {
+      timer_1 = millis();
+      ConnectWIFI();
+
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
